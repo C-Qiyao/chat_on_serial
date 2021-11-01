273402587197921ec636641be19f297e8837f340
ChenQiyao <1113938836@qq.com>
2021-11-01 10:14:38 +0800

    add decry

--- sourcecode/chat.py
+++ sourcecode/chat.py
@@ -1,3 +1,4 @@
+# coding=UTF-8
 import sys
 import serial
 import time
@@ -21,6 +22,10 @@ class MainWindow(QMainWindow,Ui_MainWindow):
 
 
 
+        self.chatbox.insertPlainText("***请先阅读遥哥忠告，并且勾选协议书\n")
+
+
+
 
 
 
@@ -29,7 +34,7 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.pushButton_login.clicked.connect(self.login)#连接串口按钮
         self.pushButton_send.clicked.connect(self.send)
         self.checkBox_zhonggao.stateChanged.connect(self.mianze)
-        print (user.user_information.username[1])
+        self.pushButton_quit.clicked.connect(self.quit)
 
 
         
@@ -48,7 +53,7 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         
         self.port_list = list(serial.tools.list_ports.comports())# 获取当前可用串口列表，serial模块函数
         if len(self.port_list) == 0:# 判断串口列表是否为空
-            self.chatbox.insertPlainText("未找到可用串口\n")# 弹出错误警告框，自建函数
+            self.chatbox.insertPlainText("***未找到可用串口\n")# 弹出错误警告框，自建函数
         else:
             for i in range(0,len(self.port_list)):# 遍历可用串口列表
                 self.comboBox.addItem(self.port_list[i][0])# 将可用串口添加至comboBox（复选框）控件
@@ -64,22 +69,23 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             try:#开启串口
                 self.serial.open()# 打开串口
                 if self.serial.is_open:# 判断串口是否打开
-                    self.chatbox.insertPlainText('外设连接成功\n')
-                    self.chatbox.insertPlainText(user.user_information.cnuser[userid]+' 欢迎登陆'+'\n')
+                    self.chatbox.clear()
+                    self.chatbox.insertPlainText('***外设连接成功\n')
+                    self.chatbox.insertPlainText(user.user_information.cnuser[userid]+' 欢迎登陆...'+'\n')
                     self.serial.is_open=1
                     self.lineEdit_name.setText(user.user_information.cnuser[userid])
                     self.pushButton_login.setEnabled(False)
                 else:
-                    self.chatbox.insertPlainText('外设连接失败\n')
+                    self.chatbox.insertPlainText('***外设连接失败\n')
             except Exception as err:
                 if self.serial.is_open==1:
                     self.chatbox.insertPlainText('外设已连接,勿重复点击\n')
                 else:
-                    self.chatbox.insertPlainText("外设连接失败,请选择未占用串口\n")
+                    self.chatbox.insertPlainText("***外设连接失败,请选择未占用串口\n")
         elif logstate==2:
-            self.chatbox.insertPlainText("密码错误\n")
+            self.chatbox.insertPlainText("***密码错误\n")
         elif logstate==3:
-            self.chatbox.insertPlainText("用户不存在\n")
+            self.chatbox.insertPlainText("***用户不存在\n")
 
     def send(self):
         if self.serial.is_open==1:
@@ -89,10 +95,11 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             strsend=strtime+">>"+nicheng+"说:"+strs+'\n'
             self.chatbox.insertPlainText(strsend)
         else:
-            self.chatbox.insertPlainText('请先连接LoRa设备\n')
+            self.chatbox.insertPlainText('***请先连接LoRa设备\n')
         if self.jiami.isChecked():
-            miwen=self.cryp_str(strs)
+            miwen=self.cryp_str(strsend)
             print(miwen)
+            print(self.decry_str(miwen))
             self.serial.write(bytes(miwen+'\n',encoding='utf-8'))
         else:
             self.serial.write(bytes(strsend,encoding='utf-8'))
@@ -114,6 +121,7 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         ciphertext_hex = b2a_hex(ciphertext) # 字符串转十六进制数据
         ciphertext_hex_de = ciphertext_hex.decode()
         return ciphertext_hex_de
+        
     def readserial(self):
         while(1):
            if self.serial.is_open==1:
@@ -123,15 +131,31 @@ class MainWindow(QMainWindow,Ui_MainWindow):
                     time.sleep(0.002)
                     continue       
 
-    def zhonggao(self):
-        self.chatbox.insertPlainText('  互联网不是法外之地。利用网络鼓吹推翻国家政权，煽动宗教极端主义，宣扬民族分裂思想，教唆暴力恐怖活动，等等，这样的行为要坚决制止和打击，决不能任其大行其道。\n  利用网络进行欺诈活动，散布色情材料，进行人身攻击，兜售非法物品，等等，这样的言行也要坚决管控，决不能任其大行其道。\n')
+    def decry_str(self,value):
+        cryptor = AES.new(self.k, self.mode, self.iv) # 创建一个AES实例
+        value_hex = a2b_hex(value) # 将十六进制数据转换为字符串
+        plain_text = cryptor.decrypt(value_hex) # 对字符串进行解密
+        return bytes.decode(plain_text).rstrip('\0')
 
+    def zhonggao(self):
+        self.chatbox.insertPlainText('  1.任何个人和组织使用网络应当遵守宪法法律，遵守公共秩序，尊重社会公德，不得危害网络安全，不得利用网络从事危害国家安全、荣誉和利益，煽动颠覆国家政权、推翻社会主义制度，煽动分裂国家、破坏国家统一，宣扬恐怖主义、极端主义，宣扬民族仇恨、民族歧视，传播暴力、淫秽色情信息，编造、传播虚假信息扰乱经济秩序和社会秩序，以及侵害他人名誉、隐私、知识产权和其他合法权益等活动。\n')
+        self.chatbox.insertPlainText('  2.禁止反向工程、反向编译和反向汇编：用户不得对本软件产品进行反向工程(Reverse Engineer)、反向编译(Decompile)或反向汇编(Disassemble)，同时不得改动编译在程序文件内部的任何资源。除法律、法规明文规定允许上述活动外，用户必须遵守此协议限制。\n')
+        self.chatbox.insertPlainText('  3.使用本软件产品风险由用户自行承担，在适用法律允许的最大范围内，对因使用或不能使用本软件所产生的损害及风险，包括但不限于直接或间接的个人损害、商业赢利的丧失、贸易中断、商业信息的丢失或任何其它经济损失，作者不承担任何责任。\n')
+        self.chatbox.insertPlainText('  4.本协议适用中华人民共和国法律。\n')
     def mianze(self):
         if self.checkBox_zhonggao.isChecked():
             self.pushButton_login.setEnabled(True)
+            self.pushButton_send.setEnabled(True)
         else:
             self.pushButton_login.setEnabled(False)
-
+            self.pushButton_send.setEnabled(False)
+    def quit(self):
+        try:
+            self.serial.flushInput()# 清除串口缓存
+            self.serial.close()# 关闭串口
+            sys.exit()
+        except Exception as err:
+            sys.exit()       
 
 
 
